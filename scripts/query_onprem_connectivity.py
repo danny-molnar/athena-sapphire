445299ac3999afdccb7f872b2c94cc673e99f85b
danny-molnar <danny.molnar@cloudreach.com>
2022-06-20 17:09:10 +0100

    add new features, new queries scripts

--- scripts/query_onprem_connectivity.py
+++ scripts/query_onprem_connectivity.py
@@ -2,9 +2,7 @@
 
 # This script will:
 # - create and run a query in Athena to check if there's any on-prem connectivity appearing in the flow logs
-# - the query will check if the srcaddr field 
-
-# Usage: $ python[3] athena.py -a ACCOUNT_NUMBER -d DATE{FORMAT: YYYY-MM-DD} -r REGION -t ATHENA_TABLE_NAME
+# - the query will check if the srcaddr field contains the on-prem cidr range 
 
 import argparse
 import boto3
@@ -21,52 +19,18 @@ class StringBuilder:
         
     def __str__(self):
         return self.string.getvalue()
-    
-log_format_lookup = {
-    "account_id"            : "string",
-    "action"                : "string",
-    "az_id"                 : "string",
-    "bytes"                 : "bigint",
-    "dstaddr"               : "string",
-    "dstport"               : "int",
-    "end"                   : "bigint",
-    "flow_direction"        : "string",
-    "instance_id"           : "string",
-    "interface_id"          : "string",
-    "log_status"            : "string",
-    "packets"               : "bigint",
-    "pkt_dst_aws_service"   : "string",
-    "pkt_dstaddr"           : "string",
-    "pkt_src_aws_service"   : "string",
-    "pkt_srcaddr"           : "string",
-    "protocol"              : "bigint",
-    "region"                : "string",
-    "srcaddr"               : "string",
-    "srcport"               : "int",
-    "start"                 : "bigint",
-    "sublocation_id"        : "string",
-    "sublocation_type"      : "string",
-    "subnet_id"             : "string",
-    "tcp_flags"             : "int",
-    "traffic_path"          : "int",
-    "type"                  : "string",
-    "version"               : "int",
-    "vpc_id"                : "string"
-}
 
-log_format = "`account_id` string, `action` string, `az_id` string, `bytes` bigint, `dstaddr` string, `dstport` int, `end` bigint, `flow_direction` string, `instance_id` string, `interface_id` string, `log_status` string, `packets` bigint, `pkt_src_aws_service` string, `pkt_dstaddr` string, `pkt_srcaddr` string, `protocol` bigint, `region` string, `srcaddr` string, `srcport` int, `start` bigint, `sublocation_id` string, `sublocation_type` string, `subnet_id` string, `tcp_flags` int, `traffic_path` int, `type` string, `version` int, `vpc_id` string"
-
-def runQueryForAllFields(table_name, date, query_output_bucket_location, workgroup="primary"):
+def runOnPremConnectionCheckQuery(table_name, date, query_output_bucket_location, workgroup="primary"):
     
     client = boto3.client('athena')
     
     query_str = StringBuilder()
-    query_str.add("SELECT * FROM ")
+    query_str.add("SELECT account_id, flow_direction, srcaddr FROM ")
     query_str.add(table_name)
     query_str.add(" WHERE date = DATE('")
     query_str.add(date)
-    query_str.add("');")
-    
+    query_str.add("')")
+
     response = client.start_query_execution(
         
         QueryString=str(query_str),
@@ -89,10 +53,9 @@ def main(table):
     
     # the below variables will temporarily hold the flow logs and output buckets' 
     # this hardcoding of resources will have to be removed and parameterised with arguments coming from calling the script
-    flow_logs_bucket = "s3://sapphire-vpc-flow-logs-834539731159/"
     query_output_bucket = "s3://sapphire-vpc-flow-logs-athena-query-results-834539731159/"
     
-    runQueryForAllFields(table, "2022-06-18", query_output_bucket)
+    runOnPremConnectionCheckQuery(table, "2022-06-18", query_output_bucket)
     
     
 def parser():
--- scripts/query_rejected_packets.py
+++ scripts/query_rejected_packets.py
@@ -1,9 +1,8 @@
-# Query all fields in Athena
+# Query Rejected Packets
 
 # This script will:
-# - create and run a query in Athena for all fields appearing in the VPC flow logs
-
-# Usage: $ python[3] query_all_fields.py -t ATHENA_TABLE_NAME
+# - create and run a query in Athena to check for rejected packages
+#
 
 import argparse
 import boto3
@@ -20,18 +19,18 @@ class StringBuilder:
         
     def __str__(self):
         return self.string.getvalue()
-    
-def runQueryForAllFields(table_name, date, query_output_bucket_location, workgroup="primary"):
+
+def runRejectedPacketsQuery(table_name, date, query_output_bucket_location, workgroup="primary"):
     
     client = boto3.client('athena')
     
     query_str = StringBuilder()
-    query_str.add("SELECT * FROM ")
+    query_str.add("SELECT account_id, flow_direction, srcaddr, action FROM ")
     query_str.add(table_name)
     query_str.add(" WHERE date = DATE('")
     query_str.add(date)
-    query_str.add("');")
-    
+    query_str.add("') AND action = 'REJECT'")
+
     response = client.start_query_execution(
         
         QueryString=str(query_str),
@@ -54,10 +53,9 @@ def main(table):
     
     # the below variables will temporarily hold the flow logs and output buckets' 
     # this hardcoding of resources will have to be removed and parameterised with arguments coming from calling the script
-    flow_logs_bucket = "s3://sapphire-vpc-flow-logs-834539731159/"
     query_output_bucket = "s3://sapphire-vpc-flow-logs-athena-query-results-834539731159/"
     
-    runQueryForAllFields(table, "2022-06-18", query_output_bucket)
+    runRejectedPacketsQuery(table, "2022-06-18", query_output_bucket)
     
     
 def parser():
